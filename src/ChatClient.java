95481ec44b1b752720034f58127e0f5f72acd25f
LUNKUS <sigitas@lunkus.com>
2020-04-16 17:48:01 +0300

    Login window done

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,3 +1,53 @@
-public class ChatClient {
+import org.w3c.dom.Text;
 
+import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+public class ChatClient extends JFrame {
+
+    String loginName;
+
+    JTextArea messages;
+    JTextField sendMessage;
+
+    JButton send;
+    JButton logout;
+
+    public ChatClient(String loginName) throws UnknownHostException, IOException {
+        super(loginName);
+        this.loginName = loginName;
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                // super.windowClosing(e);
+            }
+        });
+
+        messages = new JTextArea(18,50);
+        messages.setEditable(false);
+        sendMessage = new JTextField(50);
+
+        send = new JButton("Send");
+        logout = new JButton("Logout");
+
+        setup();
+    }
+
+    private void setup() {
+        setSize(600,400);
+
+        JPanel panel = new JPanel();
+        panel.add(new JScrollPane(messages));
+
+        panel.add(sendMessage);
+        panel.add(send);
+        panel.add(logout);
+        add(panel);
+
+        setVisible(true);
+    }
 }
--- src/Login.java
+++ src/Login.java
@@ -1,11 +1,23 @@
 import javax.swing.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.IOException;
+import java.net.UnknownHostException;
 
 public class Login {
 
     static void startChat(JFrame login, String loginName) {
-        System.out.println(loginName);
+        // System.out.println(loginName);
+
+        try {
+            ChatClient client = new ChatClient(loginName);
+            login.setVisible(false);
+            login.dispose();
+        } catch (UnknownHostException e ) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
